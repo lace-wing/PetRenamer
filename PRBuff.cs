ad2af4b3a502444eb5d1a80a4b5d8d85f3c4d50b
direwolf420 <amakejk1@web.de>
2019-04-27 13:24:16 +0200

    show name in buff tip

---
+++ PRBuff.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace PetRenamer
+{
+    class PRBuff : GlobalBuff
+    {
+        public override void ModifyBuffTip(int type, ref string tip, ref int rare)
+        {
+            PRPlayer petPlayer = Main.LocalPlayer.GetModPlayer<PRPlayer>();
+            if (Main.vanityPet[type] && petPlayer.petNameVanity != "")
+            {
+                tip += "\nName: " + petPlayer.petNameVanity;
+            }
+            else if(Main.lightPet[type] && petPlayer.petNameLight != "")
+            {
+                tip += "\nName: " + petPlayer.petNameLight;
+            }
+        }
+    }
+}
